9f77001594f20615a050cbfc36ff628cbb36734a
Alan <alan@fullworks.net>
2024-06-04 23:06:06 +0100

    Update package versions and restructure file directories
    The commit updated the 'del' dependency version in package-lock.json and package.json files from 'latest' to '6.1.1'. The directory path for several files were also changed from 'src' to 'meet-my-team'. Some scripts in gulpfile.js were removed and changes were made to .gitignore and several other files. A 'wp-env.json' and a 'composer.json' file have also been added.

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ var dirSync = require( 'gulp-directory-sync' );
 var removeLines = require('gulp-remove-lines');
 var wpPot = require('gulp-wp-pot');
 var sort = require('gulp-sort');
-var notify = require("gulp-notify");
 
 
 
@@ -47,21 +46,20 @@ gulp.task('clean', () => {
 
 gulp.task('sync', () => {
     return gulp.src('.', {allowEmpty: true})
-        .pipe(dirSync('src', 'dist', {printSummary: true}))
+        .pipe(dirSync('meet-my-team', 'dist', {printSummary: true}))
         .on('error', gutil.log);
 });
 
 gulp.task('translate', () => {
-    return gulp.src(['src/**/*.php', '!src/{vendor,vendor/**}'])
+    return gulp.src(['meet-my-team/**/*.php', '!meet-my-team/{vendor,vendor/**}'])
         .pipe(sort())
         .pipe(wpPot({
             domain: project,
             package: project
         }))
         .on('error', gutil.log)
-        .pipe(gulp.dest('src/languages/' + project + '.pot'))
-        .pipe(gulp.dest('dist/languages/' + project + '.pot'))
-        .pipe(notify({message: 'TASK: "translate" Completed! 💯', onLast: true}));
+        .pipe(gulp.dest('meet-my-team/languages/' + project + '.pot'))
+        .pipe(gulp.dest('dist/languages/' + project + '.pot'));
 
 });
 
--- meet-my-team/readme.txt
+++ meet-my-team/readme.txt
@@ -2,7 +2,7 @@
 Contributors: Buooy, Fullworks
 Tags: meet,my,team,members,staff,gallery,responsive,modal,grid
 Donate link: https://ko-fi.com/wpalan
-Tested up to: 6.1
+Tested up to: 6.5
 Stable tag: 2.0.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
